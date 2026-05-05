65fd687fb48761ef3d040ba44d3ab873ddf66b03
iprocastinate <pradyumnasarkar4@gmail.com>
2026-05-05 20:09:45 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Config:
     MONGO_URI_2 = os.environ.get("MONGO_URI_2", "mongodb+srv://hosaxi3364_db_user:VGB5NXxdieAuc9YX@cluster0.6o6xtd1.mongodb.net/?retryWrites=true&w=majority")  # Admins & Logs
     
     # Multi-Redis Setup (If needed on bot side)
-    REDIS_URI_1 = os.environ.get("REDIS_URI_1", "redis://redis-19136.crce219.us-east-1-4.ec2.cloud.redislabs.com:19136")
+    REDIS_URI_1 = os.environ.get("REDIS_URI_1", "redis://default:dKJuyhorvRzf1r5wHR8K9TaGZI30As0n@redis-19136.crce219.us-east-1-4.ec2.cloud.redislabs.com:19136")
     REDIS_URI_2 = os.environ.get("REDIS_URI_2", "redis://redis-16388.crce300.ap-south-1-2.ec2.cloud.redislabs.com:16388")
     
     # Web App URL
